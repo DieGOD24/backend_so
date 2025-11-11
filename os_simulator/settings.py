918d4c59ee5afbc03a69c19eaf8b443998936db0
DieGOD24 <diego.giraldo2@utp.edu.co>
2025-11-11 11:12:55 -0500

    Try FIX 1

--- os_simulator/settings.py
+++ os_simulator/settings.py
@@ -30,6 +30,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "rest_framework",
+    "django_filters",
+    "drf_spectacular",
+    "corsheaders",
+    
     'simulator',
     'vfs',
 ]
